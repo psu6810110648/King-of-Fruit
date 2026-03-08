d6880557588c26a8191823b2f53455647a0f4245
Thanchanok Malikaew <6810110151@psu.ac.th>
2026-03-08 19:42:11 +0700

    chore: import GridLayout widget

--- screens/level_select_screen.py
+++ screens/level_select_screen.py
@@ -5,6 +5,7 @@ from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
 from kivy.graphics import Color, RoundedRectangle, Ellipse
 from kivy.app import App
 
