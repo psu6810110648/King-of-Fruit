b7b23ff29af14e0dd7028ca83e5c99159ef43c43
Thanchanok Malikaew <6810110151@psu.ac.th>
2026-03-08 19:41:52 +0700

    chore: import ScrollView widget

--- screens/level_select_screen.py
+++ screens/level_select_screen.py
@@ -4,6 +4,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 from kivy.graphics import Color, RoundedRectangle, Ellipse
 from kivy.app import App
 
